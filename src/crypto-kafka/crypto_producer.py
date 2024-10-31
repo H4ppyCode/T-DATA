cff03c6fb617192488b1ab2b5a903381e327e05a
Pacôme <dhiser.pacome@gmail.com>
2024-10-31 14:31:56 +0100

    refactor: Update default sleep time and generate random data

--- src/crypto-kafka/crypto_producer.py
+++ src/crypto-kafka/crypto_producer.py
@@ -1,19 +1,30 @@
 import json
 import time
+import random
 import requests
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 from prometheus_client import start_http_server, Summary, Counter
 
-
-# Create Prometheus metrics
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 MESSAGES_SENT = Counter('messages_sent_total', 'Total number of messages sent')
 
-def get_crypto_data():
-    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd"
-    response = requests.get(url)
-    return response.json()
+def get_crypto_data(use_api=True):
+    if use_api:
+        url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd"
+        response = requests.get(url)
+        data = response.json()
+        print("Data fetched from API")
+    else:
+        data = {
+            "bitcoin": {"usd": random.uniform(30000, 60000)},
+            "ethereum": {"usd": random.uniform(1000, 4000)},
+            "transaction_id": random.randint(1, 1000000)
+        }
+        print("Random data generated")
+
+    data['transaction_id'] = random.randint(1, 1000000)
+    return data
 
 def on_send_success(record_metadata):
     print(f"Successfully sent message to topic: {record_metadata.topic}")
@@ -39,16 +50,15 @@ def create_producer():
         print(f"Failed to create Kafka producer: {e}")
         raise
 
-@REQUEST_TIME.time()  # Measure the time taken by this function
-def main():
+@REQUEST_TIME.time()
+def main(use_api=True):
     producer = create_producer()
     topic = 'crypto_prices'
 
-    # Start up the server to expose the metrics.
     start_http_server(8000)
 
     while True:
-        crypto_data = get_crypto_data()
+        crypto_data = get_crypto_data(use_api=use_api)
         try:
             future = producer.send(topic, crypto_data)
             future.add_callback(on_send_success).add_errback(on_send_error)
@@ -57,8 +67,8 @@ def main():
         except KafkaError as e:
             print(f"Error sending message: {e}")
             if 'RecordAccumulator is closed' in str(e):
-                producer = create_producer()  # Reinitialize the producer
-        time.sleep(20)
+                producer = create_producer()
+        time.sleep(1)
 
 if __name__ == "__main__":
-    main()
+    main(use_api=False)
